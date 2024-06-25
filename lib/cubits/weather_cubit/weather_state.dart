69dbf46a581c591f5fc569604bd904baab8ae16a
kareemkamal11 <sefi31009853@f-eng.tanta.edu.eg>
2024-06-25 13:42:06 +0300

    done

---
+++ lib/cubits/weather_cubit/weather_state.dart
@@ -0,0 +1,16 @@
+import 'package:weather_app/models/weather_model.dart';
+
+abstract class WeatherState {}
+
+class WeatherInitial extends WeatherState {}
+
+class WeatherLoading extends WeatherState {}
+
+class WeatherSuccess extends WeatherState {
+  final WeatherModel weatherModel;
+
+  WeatherSuccess(this.weatherModel);
+  
+}
+
+class WeatherFailure extends WeatherState {}
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:weather_app/cubits/weather_cubit/weather_cubit.dart';
+import 'package:weather_app/cubits/weather_cubit/weather_state.dart';
+import 'package:weather_app/models/weather_model.dart';
+import 'package:weather_app/pages/search_page.dart';
+
+import '../widgets/success_body.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // WeatherModel? weatherData;
+
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) {
+                return SearchPage();
+              }));
+            },
+            icon: const Icon(Icons.search),
+          ),
+        ],
+        title: const Text('Weather App'),
+      ),
+      body: BlocBuilder<WeatherCubit, WeatherState>(
+        builder: (context, state) {
+          if (state is WeatherLoading) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          } else if (state is WeatherSuccess) {
+            return SuccessBody(weatherData: state.weatherModel);
+          } else if (state is WeatherFailure) {
+            return const Center(
+              child: Text('there is an error 😔'),
+            );
+          } else {
+            return const DefaultBody();
+          }
+        },
+      ),
+    );
+  }
+}
+
+class DefaultBody extends StatelessWidget {
+  const DefaultBody({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(
+            'there is no weather 😔 start',
+            style: TextStyle(
+              fontSize: 30,
+            ),
+          ),
+          Text(
+            'searching now 🔍',
+            style: TextStyle(
+              fontSize: 30,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
